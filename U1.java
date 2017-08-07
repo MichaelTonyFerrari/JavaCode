986fa504442fb2b9cb9827ca617f880be203ef73
restores <ferraro2ndbase@gmail.com>
2017-08-07 17:46:52 -0400

    solved

--- U1.java
+++ U1.java
@@ -3,5 +3,32 @@
  */
 public class U1 {
 
-    
+    static boolean UnChar(String str){
+
+        // Not in possible characters via ASCII
+        if(str.length() > 128)
+            return false;
+
+        boolean[] charset = new boolean[128];
+
+        for(int i=0; i < str.length(); i++)
+        {
+            // For each location in charset, check if we already seen. If true, we have seen.
+            int val = str.charAt(i);
+            if(charset[val])
+                return false;
+
+            // Set value to true
+            charset[val] = true;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+
+        String one = "hello";
+        String two = "cat";
+
+        System.out.println(UnChar(one));
+        System.out.println(UnChar(two));
+    }
 }
